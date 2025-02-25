69a5ab0cd9f8359d795eacc291b8f4f307e52d21
Peter Morganelli <petercarlmorganelli@gmail.com>
2025-02-24 23:34:14 -0500

    updating playtest scene for kaelen

---
+++ GameDesign_Snowplow_Race/Assets/Scripts/SnowCollision.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnowCollision : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Snowpile"))
+        {
+            Debug.Log("collided!!");
+            Destroy(gameObject);
+        }
+    }
+}
