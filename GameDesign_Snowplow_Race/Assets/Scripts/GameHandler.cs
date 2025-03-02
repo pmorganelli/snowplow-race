e173dfabb8050c8af6f7c2b0b97d8e867ffe2e4a
v1v14nb <156724407+v1v14nb@users.noreply.github.com>
2025-03-02 15:45:17 -0500

    Revert "Merge branch 'main' of https://github.com/pmorganelli/snowplow-race"
    This reverts commit ee51ba0b1a058a69a15220a37e621c18f1e2bded, reversing
    changes made to 976da9c73d9e855749ad9132abb409793b895b03.

--- GameDesign_Snowplow_Race/Assets/Scripts/GameHandler.cs
+++ GameDesign_Snowplow_Race/Assets/Scripts/GameHandler.cs
@@ -2,92 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.SceneManagement;
+using UnityEngine.SceneManagement; //change #1: added namespace
+
+
 
 public class GameHandler : MonoBehaviour
 {
     public GameObject scoreText;
-    public GameObject timeText;
-    public int playerScore = 0;
-    public static int scoreToWin = 850;
-    public GameObject textFinalScore;
-
-    // Game time in seconds
-    public int gameTime = 120;
-    // Internal timer to accumulate deltaTime
-    private float gameTimer = 0f;
+    public static int playerScore = 0;
+    public int scoreToWin = 200;
 
     // Start is called before the first frame update
     void Start()
     {
-        UpdateTime();
         UpdateScore();
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        // Only update timer if we're not in the EndScene
-        if (SceneManager.GetActiveScene().name != "EndScene")
-        {
-            gameTimer += Time.deltaTime;
-            if (gameTimer >= 1f)
-            {
-                gameTimer -= 1f;
-                gameTime--;
-                UpdateTime();
-
-                // If time is up, load the EndScene
-                if (gameTime <= 0)
-                {
-                    SceneManager.LoadScene("EndScene");
-                }
-            }
-        }
-    }
-
     public void AddScore(int points)
     {
         playerScore += points;
         UpdateScore();
     }
 
-    public void UpdateTime()
-    {
-        Text timeTextB = timeText.GetComponent<Text>();
-        int minutes = gameTime / 60;
-        int seconds = gameTime % 60;
-        timeTextB.text = "Time Left: " + minutes.ToString() + ":" + seconds.ToString("D2");
-    }
-
-
+    // Update is called once per frame
     void UpdateScore()
     {
-        if (SceneManager.GetActiveScene().name == "EndScene")
+        Text scoreTextB = scoreText.GetComponent<Text>();
+        if(SceneManager.GetActiveScene().name == "EndScene")
         {
-            Text finalScoreText = textFinalScore.GetComponent<Text>();
-            finalScoreText.text = "FINAL SCORE: " + playerScore;
+            scoreTextB.text = "FINAL SCORE: " + playerScore;
         }
-        else
-        {
-            Text scoreTextB = scoreText.GetComponent<Text>();
-            scoreTextB.text = "Score: " + playerScore;
 
-            //check for winning condition
-            if (playerScore >= scoreToWin)
-            {
-                // playerScore = 0;
-                SceneManager.LoadScene("EndScene");
-            }
+        if (playerScore >= scoreToWin)
+        {
+            //scoreTextB.text = "FINAL SCORE:" + playerScore;
+            playerScore = 0;
+            SceneManager.LoadScene("EndScene"); //change #2: switch scene
+                                                //gameOverText.SetActive(true); //change #3: comment gameOverText
         }
+        scoreTextB.text = "SCORE: " + playerScore;
     }
-
     void RestartGame()
     {
         playerScore = 0;
         SceneManager.LoadScene("peterPlaytestSnowplow");
     }
-
     void QuitGame()
     {
 #if UNITY_EDITOR
