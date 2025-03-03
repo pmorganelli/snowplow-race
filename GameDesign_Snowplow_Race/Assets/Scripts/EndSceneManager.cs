025a849f7476423a58bea4eadd0ae54b0c4eeed4
Norman Ramsey <nr@cs.tufts.edu>
2025-03-02 20:06:02 -0500

    remove all functionality from EndSceneManager
    that functionality duplicated functionality on GameHandler

--- GameDesign_Snowplow_Race/Assets/Scripts/EndSceneManager.cs
+++ GameDesign_Snowplow_Race/Assets/Scripts/EndSceneManager.cs
@@ -6,32 +6,5 @@ using UnityEngine.SceneManagement;
 
 public class EndSceneManager : MonoBehaviour
 {
-    public static int playerScore = 0;
-    // Start is called before the first frame update
-    public void RestartGame()
-    {
-        playerScore = 0;
-        SceneManager.LoadScene("SelinScene");
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if(playerScore == 20) {
-            EndGame();
-        }
-    }
-
-    void EndGame() {
-        playerScore = 0;
-        SceneManager.LoadScene("EndScene");
-    }
-    public void QuitGame() {
-        #if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;
-        #else
-        Application.Quit();
-        #endif
-    }
 
 }
